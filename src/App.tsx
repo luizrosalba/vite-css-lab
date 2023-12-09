9ccf35844634a0893cfd96f330aec6c9fd83e2e0
Luiz Rosalba <luizrosalba@gmail.com>
2023-12-09 13:51:17 -0300

    Refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import './App.scss'
+import Footer from './components/Footer'
+import Header from './components/Header'
 
 function App() {
 
   return (
     <>
-      <h1>VITE + REACT + TS + SCSS </h1>
+      <Header />
+      <Footer />
     </>
+
   )
 }
 
