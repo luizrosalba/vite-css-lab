4229692ceb61111ed41da03205a9f33e129d36a5
luizrosalba <luizrosalba@gmail.com>
2023-12-10 12:52:40 -0300

    some examples

--- src/components/Selectors/Selectors.tsx
+++ src/components/Selectors/Selectors.tsx
@@ -4,7 +4,14 @@ export default function Selectors() {
         <>
             <h2>Selectors</h2>
             <p className='text'>Selectors Examples . </p>
-            <div><p>Example Nested</p></div>
+            <a href="https://www.w3schools.com/cssref/css_selectors.php">Reference </a>
+            <div className="class-example">
+                <div className="class-example1">
+                    I'm descendant
+                </div>
+                I'm red
+            </div>
+            <div className="class-example2"> I'm blue </div>
         </>
     )
 }
